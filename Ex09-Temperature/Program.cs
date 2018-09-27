682dd98c9a88fc3c74a762b2b73cbac15b57f8d3
Pete59f1 <pete59f1@edu.eal.dk>
2018-09-27 12:22:15 +0200

    Add project files.

---
+++ Ex09-Temperature/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex09_Temperature
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            int counter = 0;
+            Temperature tempe = new Temperature();
+
+            System.IO.StreamReader file = new System.IO.StreamReader(@"C:\Users\Peter\Documents\Visual Studio 2017\Projects\Ex09-Temperature\Ex09-Temperature\InputFil.txt");
+            while((line = file.ReadLine()) != null)
+            {
+                string[] split = line.Split(' ');
+                counter++;
+                if(counter == 1)
+                {
+                    for(int i = 0; i < split.Length; i++)
+                    {
+                        tempe.Celsius = double.Parse(split[i]);
+                        Console.WriteLine(tempe.Celsius);
+                    }
+                }
+                Console.WriteLine("");
+                
+                if(counter == 2)
+                {
+                    for (int i = 0; i < split.Length; i++)
+                    {
+                        tempe.Fahrenheit = double.Parse(split[i]);
+                        Console.WriteLine(tempe.Fahrenheit);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Ex09-Temperature/TemperatureUtil.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex09_Temperature
+{
+    public class TemperatureUtil
+    {
+        public static double FahrenheitToCelsius(double v)
+        {
+            return  (v - 32) * 5 / 9;
+        }
+
+        public static double CelsiusToFahrenheit(double v)
+        {
+            return (9 * v) / 5 + 32;
+        }
+    }
+}
